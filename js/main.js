f4b6feaafcd5fd2f6c6651d2cd81f90ce2474b23
Andrew Lawrence <alaw16@gmail.com>
2016-08-16 11:32:03 -0700

    updates

--- js/main.js
+++ js/main.js
@@ -2,11 +2,58 @@ var words = [];
 words[0] = ['Budding','Accomplished']; 
 words[1] = ['Curious','Certain']; 
 words[2] = ['Realistic','Idealistic']; 
-words[3] = ['Practicle','Imaginative']; 
+words[3] = ['Practicle','Imaginative'];
 
-// var i = $("input[name^='inputfield']");
+// function random_sort () {
+//       return (0.5 - Math.random() );
+// }
+// words.sort(random_sort);
 
-// i.append( "Test" );
-var i = $("#input");
+var i = 0,
+	n = words.length;
+var arent = [],
+	are = [];
 
-i.append(words);
+$("#forward").data('dir', 1);
+$("#back").data('dir', -1);
+
+$("#forward, #back").on('click', function() {
+	i = (i + $(this).data('dir') + n) % n;
+	$("#input").hide().html(words[i][0]).fadeIn(200);
+	$("#input2").hide().html(words[i][1]).fadeIn(200);
+});
+
+$("#flip").on('click', function() {
+	$("#input").toggleClass('switchout push');
+	$("#input2").toggleClass('switchout push');
+});
+
+$("#back").trigger('click'); //initialize
+
+
+
+$("#no").on('click', function() {
+	var lastArent = arent[arent.length-1];
+	//removing the current index value from the array
+	words.splice(i,1);
+
+	//push content of main > p to arent
+	arent.push($('.push').text());
+
+	$('#arent').append('<li>' + lastArent + '</li>');
+});
+
+   
+
+$("#yes").on('click', function() {
+	var lastAre = are[are.length-1];
+	//removing the current index value from the array
+	words.splice(i,1);
+
+	//push content of main > p to arent
+	are.push($('main p').text());
+
+	$('#are').append('<li>' + lastAre + '</li>');
+});
+
+
